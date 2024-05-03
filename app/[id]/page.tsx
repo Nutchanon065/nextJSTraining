8660cc0dab88c14abe07338860b79e0de16a66c8
Tle <natchanon0832@gmail.com>
2024-05-03 16:39:24 +0700

    first commit

---
+++ app/[id]/page.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { IUser } from '@/types/IUser'
+import axios from 'axios'
+import { useParams } from 'next/navigation'
+import React, { useEffect, useState } from 'react'
+
+type Props = {}
+
+function page({}: Props) {
+    const param = useParams<{id : string}>();
+    const [dataById, setDataById] = useState<IUser | null>(null);
+
+    const getDataById = async () => {
+        const response = await axios.get(`https://663489d79bb0df2359a1cfe6.mockapi.io/api/v1/users/${param.id}`)
+
+        setDataById(response.data)
+    }
+
+    useEffect(() => {
+        getDataById();
+    }, [])
+
+  return (
+    <>
+        <div style={{margin: '10px'}}>
+            <h2>Detail</h2>
+            <label>First name : </label> {dataById?.first_name} <br />
+            <label>Last name : </label> {dataById?.last_name} <br />
+            <label>Phone number : </label> {dataById?.phone_number} <br />
+            <label>Email : </label> {dataById?.email} <br />
+        </div>
+    </>
+  )
+}
+
+export default page
---
+++ app/contact/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+type Props = {}
+
+function page({}: Props) {
+  return (
+    <div style={{textAlign:"center"}}>
+      <h1 style={{fontSize: 30}}>Contact</h1>
+      <b>ณัฐชนน ร้อนครบุรี</b><br />
+      <b>เติ้ล</b><br />
+      <b>Email: nutchanon_r@protossgroup.com</b><br />
+      <b>Phone: 065-607-2964</b><br />
+      <b>Discord: Nutchanon #6813</b><br />
+    </div>
+  )
+}
+
+export default page
